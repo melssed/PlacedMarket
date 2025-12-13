298c2472a9db68c5dac3f0d93d5b3ee87a55260d
melssed <ukiteryy@mail.ru>
2025-12-13 11:42:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,19 +7,23 @@
 
 <script>
 const tg = window.Telegram.WebApp;
-const user = tg.initDataUnsafe.user;
+const user = tg.initDataUnsafe?.user;
 
 document.getElementById("form").addEventListener("submit", async (e) => {
   e.preventDefault();
 
+  const text = e.target.text.value;
+
   await fetch("/send", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({
-      text: e.target.text.value,
-      name: user.first_name,
-      username: user.username || "нет",
-      user_id: user.id
+      text: text,
+      name: user?.first_name,
+      username: user?.username,
+      user_id: user?.id
     })
   });
 
